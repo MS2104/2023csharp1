c2a70db1b189695672f599b3ece7c78cf8a31660
Milad Sahar <31002@ma-web.nl>
2023-03-10 10:22:02 +0100

    mfw

---
+++ Variables_Extra_HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Variables_Extra_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            typeOut("Hello World!");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static void typeOut(string str)
+        {
+            foreach(char c in str)
+            {
+                Console.Write(c);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
