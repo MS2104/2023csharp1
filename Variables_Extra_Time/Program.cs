edf81f3a9833fdc2659e7a3b22f35b59d37473d5
Milad Sahar <31002@ma-web.nl>
2023-03-10 10:10:52 +0100

    time.md af

---
+++ Variables_Extra_Time/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Variables_Extra_Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+                Thread.Sleep(1000);
+                Console.Clear();
+            } 
+        }
+    }
+}
