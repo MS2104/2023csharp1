c24caca6f28c367fa27f073aa099b75368e19d23
MS2104 <31002@ma-web.nl>
2023-03-16 12:43:13 +0100

    ifopdracht klaar

---
+++ IfOpdracht/IfOpdracht4/IfOpdracht4/EnemyCollider.cs
@@ -0,0 +1,30 @@
+﻿namespace IfOpdracht4
+{
+    internal class EnemyCollider
+    {
+        internal bool CanPassWall(Enemy enemy)
+        {
+            // deze opdracht was best moeilijk omdat de comments moeilijk waren te begrijpen
+            // heb meerdere mensen die kunnen vouchen hiervoor
+            if (enemy.flying || enemy.incoporial || enemy.subterranian)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        internal bool CanPassWater(Enemy enemy)
+        {
+            if (enemy.flying || enemy.incoporial || enemy.subterranian)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
