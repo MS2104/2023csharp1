6693a24570bc11343ae13dcfe7ee830a3218313e
MS2104 <31002@ma-web.nl>
2023-03-17 20:31:32 +0100

    extra classes opdrachten klaar ezzzzz

---
+++ extra_class_cs1/Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            Rectangle rect = new Rectangle();
+
+            rect.x = 100;
+            rect.y = 200;
+            rect.width = 50;
+            rect.height = 50;
+
+            Console.WriteLine(rect.x);
+            Console.WriteLine(rect.y);
+            Console.WriteLine(rect.width);
+            Console.WriteLine(rect.height);
+        }
+    }
+}
---
+++ extra_class_cs1/Constructors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Rectangle
+    {
+        // deze opdracht zou in 10 minuten gedaan moeten zijn maar ik ben gewoon zo slecht in lezen
+        // het duurde me 40 minuten :((((((
+
+        private int x, y, width, height;
+        public Rectangle(int x, int y, int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+
+        public void RectInfo()
+        {
+            Console.WriteLine($"The rectangle is located at x{x} y{y}. It is {height} pixels long and {width} pixels wide.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(100, 200, 50, 50);
+            r.RectInfo();
+        }
+    }
+}
