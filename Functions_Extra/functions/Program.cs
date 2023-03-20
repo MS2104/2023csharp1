3db1b7a5541363492e371a54e0122f9dd63c896e
MS2104 <31002@ma-web.nl>
2023-03-20 17:58:10 +0100

    functions extra 1 af

---
+++ Functions_Extra/functions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace functionsOpdracht // Ik ben nou eenmaal slecht met namen
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int nummer = 14;
+            string tekst = "Hello World!";
+
+            IkBenSlechtMetNamen(nummer, tekst);
+        }
+
+        static void IkBenSlechtMetNamen(int nummer, string tekst)
+        {
+            Console.WriteLine(tekst);
+            Console.WriteLine(nummer);
+        }
+    }
+}
