42773b4461df817f057045f7ff3c61443fd8eb12
MS2104 <31002@ma-web.nl>
2023-03-20 18:38:47 +0100

    of ik kan niet lezen of ik begrijp HELEMAAL niks van deze opdracht!?!?!?!!??!??!?

---
+++ Functions_Extra/functions_depth/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace functions_depth
+{
+    class Program
+    {
+        string text = "Hello World!";
+        int getal = 14;
+        bool mewhenthe = true;
+
+        static void Main(string[] args)
+        {
+            StringFunctie1(tekst, getal);
+        }
+        static void StringFunctie1(string tekst)
+        {
+            StringFunctie2();
+        }
+
+        static void StringFunctie2(string tekst, int getal)
+        {
+            StringFunctie3();
+        }
+
+        static void StringFunctie3(string tekst, int getal, bool mewhenthe)
+        {
+            if (mewhenthe)
+            {
+                Console.WriteLine(tekst);
+                Console.WriteLine(getal);
+                Console.WriteLine(mewhenthe);
+            }
+        }
+    }
+}
