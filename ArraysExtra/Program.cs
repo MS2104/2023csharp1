fc324935723a831eb97bb0a6413eb129449cdec9
MS2104 <31002@ma-web.nl>
2023-03-22 14:34:25 +0100

    arrays extra ook klaar lfg

---
+++ ArraysExtra/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] supercreativearrayname = {0, 10, 20, 30, 40, 50, 60};
+
+            int sum = Baller(supercreativearrayname);
+
+            Console.WriteLine(sum);
+        }
+
+        static int Baller(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+    }   
+}
